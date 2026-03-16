299a41c2efce5891efc5b782c8f7c1d00c1fc82f
Arthur <hauguel.arthur@gmail.com>
2026-03-16 08:19:04 +0100

    tests 31km ca

--- demo/dataset2load.jl
+++ demo/dataset2load.jl
@@ -1,11 +1,11 @@
 # Assignation des différents chemins utiles
-data_folder_precise = "data/raw_monthly_combined/precise"
-data_folder_basic ="data/raw_monthly_combined/basic"
+data_folder_precise = "data/france/france_9"
+data_folder_basic ="data/france/france_9_31"
 data_folder_ca_9="src/era5_land_ca_t2m"
 data_folder_ca_31="src/era5_ca_t2m_31km"
-weight_prop_basic = "data/masks/weights_prop_basic.nc"
-weight_prop_precise = "data/masks/weights_prop_precise.nc"
-weight_canada_9="data/masks/weights_canada.nc"
+weight_prop_basic = "data/masks/weights_france_31.nc"
+weight_prop_precise = "data/masks/weights_france_9.nc"
+weight_canada_9="data/masks/weights_canada_9.nc"
 weight_canada_31="data/masks/weights_canada_31.nc"
 
 # Importation des poids
