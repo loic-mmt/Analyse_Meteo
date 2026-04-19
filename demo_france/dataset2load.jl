e93de79cfd6ff5f464a1047cff52930624cf6f21
MargotL3869 <margot.laignel8@gmail.com>
2026-04-19 19:00:18 +0200

    Ajout analyse saisonnière comparative FR/CA et graphiques d'anomalies

--- demo_france/dataset2load.jl
+++ demo_france/dataset2load.jl
@@ -2,7 +2,7 @@
 data_folder_precise = "data/france/france_9"
 data_folder_basic ="data/france/france_31"
 data_folder_ca_9="src/era5_land_ca_t2m"
-data_folder_ca_31="src/era5_ca_t2m_31km"
+data_folder_ca_31 = "src/ca_31km/era5_ca_t2m_31km"
 weight_prop_basic = "data/masks/weights_france_31.nc"
 weight_prop_precise = "data/masks/weights_france_9.nc"
 weight_canada_9="data/masks/weights_canada_9.nc"
